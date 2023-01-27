bda2c276122843c96fcff4ccb4b78248652c5ded
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-27 13:56:24 +0100

    refactor code

--- src/main/java/com/example/stream/Main02.java
+++ src/main/java/com/example/stream/Main02.java
@@ -10,21 +10,20 @@ public class Main02 {
                 new Employee("John", "Niran", 'M', LocalDate.of(2000, 06, 03), 5000),
                 new Employee("James", "Carter", 'M', LocalDate.of(1990, 02, 28), 3000),
                 new Employee("Katy", "Nis", 'F', LocalDate.of(1980, 06, 03), 4000),
-                new Employee("Natalie", "Road", 'F', LocalDate.of(1970, 06, 03), 2000)
-        );
+                new Employee("Natalie", "Road", 'F', LocalDate.of(1970, 06, 03), 2000));
 
         list.stream()
-        .filter(e -> e.getSurname().startsWith("N"))
-        .forEach(e -> System.out.println(e.getName() + " " + e.getSurname()));
+                .filter(e -> e.getSurname().startsWith("N"))
+                .forEach(e -> System.out.println(e.getName() + " " + e.getSurname()));
 
         list.stream()
-        .filter(e -> e.getBirthDay().isBefore(LocalDate.now().minusYears(30)))
-        .filter(e -> e.getBirthDay().isAfter(LocalDate.now().minusYears(45)))
-        .forEach(e -> System.out.println(e.getName() + " " + e.getSurname()));
+                .filter(e -> e.getBirthDay().isBefore(LocalDate.now().minusYears(30)))
+                .filter(e -> e.getBirthDay().isAfter(LocalDate.now().minusYears(45)))
+                .forEach(e -> System.out.println(e.getName() + " " + e.getSurname()));
 
         list.stream()
-        .filter(e -> e.getSex().equals('F'))
-        .filter(e -> e.getSalary() < 3500)
-        .forEach(e -> e.setSalary(e.getSalary() * 1.1));
+                .filter(e -> e.getSex().equals('F'))
+                .filter(e -> e.getSalary() < 3500)
+                .forEach(e -> e.setSalary(e.getSalary() * 1.1));
     }
 }
