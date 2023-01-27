86119de8e5d222ec07db24af938d74278863ac00
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-27 11:59:55 +0100

    add task4 lambda

---
+++ src/main/java/com/example/lambda/Main04.java
@@ -0,0 +1,22 @@
+package com.example.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Main04 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("James");
+        list.add("John");
+        list.add("Katrine");
+        Consumer<List<String>> consumer = l -> printNames(l);
+        consumer.accept(list);
+    }
+
+    public static void printNames(List<String> names){
+        for (String s : names){
+            System.out.println(s);
+        }
+    }
+}
