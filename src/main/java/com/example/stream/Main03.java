046286bf70f7ae33ca3bb18480a339ea50fdee38
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-27 14:12:59 +0100

    refactor code

--- src/main/java/com/example/stream/Main03.java
+++ src/main/java/com/example/stream/Main03.java
@@ -9,9 +9,9 @@ public class Main03 {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
         List<Integer> numbers2 = numbers.stream()
-        .map(n -> n * n - 5)
-        .filter(n -> n < 20)
-        .collect(Collectors.toList());
+                .map(n -> n * n - 5)
+                .filter(n -> n < 20)
+                .collect(Collectors.toList());
         numbers2.forEach(System.out::println);
     }
 }
